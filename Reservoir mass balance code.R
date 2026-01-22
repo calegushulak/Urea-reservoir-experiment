6feebfe0a0a54c3f2575605bb947f1de486014fe
Gushulak Lab <75274974+calegushulak@users.noreply.github.com>
2026-01-21 21:18:22 -0600

    Add files via upload

--- Reservoir mass balance code.R
+++ Reservoir mass balance code.R
@@ -11,11 +11,11 @@ library (cowplot)
 dat <- read.csv(file.choose(), header=TRUE)
 
 ### N species
-Nitrogen <- c('NH4_g', 'NO3.NO2_g', 'DON_g', 'PON_g')
+Nitrogen <- c('NH4_g_m2', 'NO3.NO2_g_m2', 'DON_g_m2', 'PON_g_m2')
 
 ### Pivot data
 n.long <-
-  select(dat, Site, DOY, Added_urea_sum, all_of(Nitrogen)) %>%
+  select(dat, Site, DOY, Added_urea_surface_area, all_of(Nitrogen)) %>%
   pivot_longer(all_of(Nitrogen), names_to = 'Nitrogen', values_to ='mass')
 
 ### Plot individual reservoirs
@@ -23,11 +23,12 @@ F1u.p <- ggplot(data=subset(n.long, Site=="F1"), aes(fill=Nitrogen, y=mass, x=DO
   geom_vline(data=n.long, aes(xintercept=200), color ="black", size =0.5, linetype=2)+ 
   geom_bar(position="stack", stat="identity")+ ### measured TN pools+
   scale_fill_manual(values=c('#CC9900', '#0000CC','#660099', '#009933'))+ 
-  geom_point(aes(x=DOY, y=Added_urea_sum/5), pch=21, color="black", fill="#CC0000", size=1)+
-  geom_line(aes(x=DOY, y=Added_urea_sum/5), linetype=1, color="#CC0000", size=1)+
-  scale_y_continuous(sec.axis = sec_axis(transform=~.*4, name="Sum of added urea (g)"))+
+  geom_point(aes(x=DOY, y=Added_urea_surface_area/4), pch=21, color="black", fill="#CC0000", size=1)+
+  geom_line(aes(x=DOY, y=Added_urea_surface_area/4), linetype=1, color="#CC0000", size=1)+
+  scale_y_continuous(sec.axis = sec_axis(transform=~.*4, name=
+                                           expression(Sum~of~added~urea~(g~m^-2))))+
   xlab("Day of Year")+
-  ylab(expression(Nitrogen~mass~(g~reservoir^-1)))+
+  ylab(expression(Nitrogen~mass~(g~m^-2)))+
   ggtitle('F1')+
   theme_bw()
 F1u.p
@@ -36,11 +37,12 @@ F2u.p <- ggplot(data=subset(n.long, Site=="F2"), aes(fill=Nitrogen, y=mass, x=DO
   geom_vline(data=n.long, aes(xintercept=200), color ="black", size =0.5, linetype=2)+ 
   geom_bar(position="stack", stat="identity")+ ### measured TN pools+
   scale_fill_manual(values=c('#CC9900', '#0000CC','#660099', '#009933'))+ 
-  geom_point(aes(x=DOY, y=Added_urea_sum/4), pch=21, color="black", fill="#CC0000", size=1)+
-  geom_line(aes(x=DOY, y=Added_urea_sum/4), linetype=1, color="#CC0000", size=1)+
-  scale_y_continuous(sec.axis = sec_axis(transform=~.*4, name="Sum of added urea (g)"))+
+  geom_point(aes(x=DOY, y=Added_urea_surface_area/2), pch=21, color="black", fill="#CC0000", size=1)+
+  geom_line(aes(x=DOY, y=Added_urea_surface_area/2), linetype=1, color="#CC0000", size=1)+
+  scale_y_continuous(sec.axis = sec_axis(transform=~.*2, name=
+                                           expression(Sum~of~added~urea~(g~m^-2))))+
   xlab("Day of Year")+
-  ylab(expression(Nitrogen~mass~(g~reservoir^-1)))+
+  ylab(expression(Nitrogen~mass~(g~m^-2)))+
   ggtitle('F2')+
   #ylim(0,22000)+
   theme_bw()
@@ -50,11 +52,12 @@ F3u.p <- ggplot(data=subset(n.long, Site=="F3"), aes(fill=Nitrogen, y=mass, x=DO
   geom_vline(data=n.long, aes(xintercept=200), color ="black", size =0.5, linetype=2)+ 
   geom_bar(position="stack", stat="identity")+ ### measured TN pools+
   scale_fill_manual(values=c('#CC9900', '#0000CC','#660099', '#009933'))+ 
-  geom_point(aes(x=DOY, y=Added_urea_sum/5), pch=21, color="black", fill="#CC0000", size=1)+
-  geom_line(aes(x=DOY, y=Added_urea_sum/5), linetype=1, color="#CC0000", size=1)+
-  scale_y_continuous(sec.axis = sec_axis(transform=~.*4, name="Sum of added urea (g)"))+
+  geom_point(aes(x=DOY, y=Added_urea_surface_area/5), pch=21, color="black", fill="#CC0000", size=1)+
+  geom_line(aes(x=DOY, y=Added_urea_surface_area/5), linetype=1, color="#CC0000", size=1)+
+  scale_y_continuous(sec.axis = sec_axis(transform=~.*5, name=
+                                           expression(Sum~of~added~urea~(g~m^-2))))+
   xlab("Day of Year")+
-  ylab(expression(Nitrogen~mass~(g~reservoir^-1)))+
+  ylab(expression(Nitrogen~mass~(g~m^-2)))+
   ggtitle('F3')+
   #ylim(0,22000)+
   xlim(148,250)+
@@ -64,14 +67,16 @@ F3u.p
 R1.p<- ggplot(data=subset(n.long, Site=="R1"), aes(fill=Nitrogen, y=mass, x=DOY))+
   geom_bar(position="stack", stat="identity")+ ### measured TN pools+
   scale_fill_manual(values=c('#CC9900', '#0000CC','#660099', '#009933'))+ 
-  labs(title="R1", x="Day of Year", y="Mass (mg)")+
+  labs(title="R1", x="Day of Year")+
+  ylab(expression(Nitrogen~mass~(g~m^-2)))+
   theme_bw()
 R1.p
 
 R2.p <- ggplot(data=subset(n.long, Site=="R2"), aes(fill=Nitrogen, y=mass, x=DOY))+
   geom_bar(position="stack", stat="identity")+ ### measured TN pools+
   scale_fill_manual(values=c('#CC9900', '#0000CC','#660099', '#009933'))+ 
-  labs(title="R2", x="Day of Year", y="Mass (mg)")+
+  labs(title="R2", x="Day of Year")+
+  ylab(expression(Nitrogen~mass~(g~m^-2)))+
   xlim(148,250)+
   theme_bw()
 R2.p
@@ -79,7 +84,8 @@ R2.p
 R3.p <- ggplot(data=subset(n.long, Site=="R3"), aes(fill=Nitrogen, y=mass, x=DOY))+
   geom_bar(position="stack", stat="identity")+ ### measured TN pools+
   scale_fill_manual(values=c('#CC9900', '#0000CC','#660099', '#009933'))+ 
-  labs(title="R3", x="Day of Year", y="Mass (mg)")+
+  labs(title="R3", x="Day of Year")+
+  ylab(expression(Nitrogen~mass~(g~m^-2)))+
   theme_bw()
 R3.p
 
